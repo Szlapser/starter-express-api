76b9eca616c23acb56e1e88a8c04d2df2214ed6c
Szlapser <127340391+Szlapser@users.noreply.github.com>
2023-03-08 20:31:46 +0100

    Update discordstrategy.js

--- strategies/discordstrategy.js
+++ strategies/discordstrategy.js
@@ -1,6 +1,7 @@
 const DiscordStrategy = require('passport-discord').Strategy;
 const passport = require('passport')
 const DiscordUser = require('../models/DiscordUser')
+require('dotenv').config()
 
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
